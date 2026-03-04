f69b27860eb5859091ff3a8e912a92cdf703ce9b
sayan9r <sayan18082005@gmail.com>
2026-03-04 18:45:21 +0530

    checking the backend on postman

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,8 +4,14 @@ import pool from '../config/db.js';
 
 const router = express.Router();
 
-
-
+router.get("/",async(req,res) => {
+  try{
+   res.send("hello");
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+})
 
 router.post("/response", async (req, res) => {
   try {
